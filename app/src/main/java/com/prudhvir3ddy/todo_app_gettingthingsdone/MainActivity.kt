12537adeed8fdd795cc3ed5d5e1c4fb88a8f60de
prudhvi reddy <prudhvireddy.m01@gmail.com>
2020-04-04 12:12:42 +0530

    project init

---
+++ app/src/main/java/com/prudhvir3ddy/todo_app_gettingthingsdone/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.prudhvir3ddy.todo_app_gettingthingsdone
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
