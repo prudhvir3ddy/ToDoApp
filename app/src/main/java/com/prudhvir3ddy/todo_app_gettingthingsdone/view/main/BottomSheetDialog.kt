ac863dd4452cb6a7a18141d446676e7269eb2cb1
Meet K Parmar <meetkumar.k.parmar@gmail.com>
2020-05-10 20:34:51 +0530

    Feat(#16) - Improve the save task button (#28)
    * Feat(#16) - Improve the save task button
    
    * Change TextView to Button for save
    
    * Change id of save button as you suggest
    
    * Solve the error

--- app/src/main/java/com/prudhvir3ddy/todo_app_gettingthingsdone/view/main/BottomSheetDialog.kt
+++ app/src/main/java/com/prudhvir3ddy/todo_app_gettingthingsdone/view/main/BottomSheetDialog.kt
@@ -2,14 +2,18 @@ package com.prudhvir3ddy.todo_app_gettingthingsdone.view.main
 
 import android.content.Context
 import android.os.Bundle
+import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.prudhvir3ddy.todo_app_gettingthingsdone.R.layout
+import kotlinx.android.synthetic.main.dialog.save_btn
 import kotlinx.android.synthetic.main.dialog.task_desc_et
 import kotlinx.android.synthetic.main.dialog.task_name_et
-import kotlinx.android.synthetic.main.dialog.view.save_tv
+import kotlinx.android.synthetic.main.dialog.view.save_btn
+import kotlinx.android.synthetic.main.dialog.view.task_desc_et
 
 class BottomSheetDialog : BottomSheetDialogFragment() {
 
@@ -22,7 +26,8 @@ class BottomSheetDialog : BottomSheetDialogFragment() {
   ): View? {
 
     val v = inflater.inflate(layout.dialog, container, false)
-    v.save_tv.setOnClickListener {
+    setOnEditorAction(v)
+    v.save_btn.setOnClickListener {
       val taskName = task_name_et.text.toString()
       val taskDesc = task_desc_et.text.toString()
 
@@ -33,6 +38,16 @@ class BottomSheetDialog : BottomSheetDialogFragment() {
 
     }
     return v
+  }
+
+  private fun setOnEditorAction(v: View) {
+    v.task_desc_et.setOnEditorActionListener { v, actionId, event ->
+      if (event != null && event.keyCode === KeyEvent.KEYCODE_ENTER || actionId == EditorInfo.IME_ACTION_DONE) {
+        save_btn.performClick()
+        true
+      }
+      false
+    }
   }
 
   override fun onAttach(context: Context) {
