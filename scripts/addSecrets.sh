b1eb33ded66b3f1edf36abe91164c8beee6081f1
prudhvir3ddy <prudhvi.reddy@zomato.com>
2020-11-10 13:19:56 +0530

    renamed todoapp.keystore to release.keystore

--- scripts/addSecrets.sh
+++ scripts/addSecrets.sh
@@ -1,3 +1,2 @@
-mkdir "app/keystores"
 echo "$GOOGLE_SERVICES" >> ./app/google-services.json
-echo "$KEY_STORE" | base64 --decode >> ./app/keystores/todoapp.keystore
+echo "$KEY_STORE" | base64 --decode >> ./app/keystore/release.keystore
