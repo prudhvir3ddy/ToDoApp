701dccd0c5684094f306faa5ff76a07b0d4eaa61
prudhvir3ddy <prudhvireddy.m01@gmail.com>
2021-01-03 13:29:26 +0530

    migrated to kotlin parcelize

--- app/src/main/java/com/prudhvir3ddy/todo_app_gettingthingsdone/storage/db/ToDo.kt
+++ app/src/main/java/com/prudhvir3ddy/todo_app_gettingthingsdone/storage/db/ToDo.kt
@@ -3,7 +3,7 @@ package com.prudhvir3ddy.todo_app_gettingthingsdone.storage.db
 import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 import java.util.UUID
 
 @Parcelize
