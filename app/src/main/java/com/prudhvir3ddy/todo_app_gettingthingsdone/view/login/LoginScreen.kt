101ed1708b54bbb3a7d036892cb382e983c318e7
prudhvir3ddy <prudhvi.reddy@target.com>
2021-08-10 19:53:36 +0530

    fixed the import

--- app/src/main/java/com/prudhvir3ddy/todo_app_gettingthingsdone/view/login/LoginScreen.kt
+++ app/src/main/java/com/prudhvir3ddy/todo_app_gettingthingsdone/view/login/LoginScreen.kt
@@ -20,7 +20,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextAlign.Companion
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.prudhvir3ddy.todo_app_gettingthingsdone.R.mipmap
@@ -80,7 +79,7 @@ fun PlanText(
 
   Text(
     text = text,
-    textAlign = Companion.Start,
+    textAlign = TextAlign.Start,
     style = MaterialTheme.typography.h6,
     modifier = modifier
       .fillMaxWidth()
