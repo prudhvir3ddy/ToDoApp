26879469946aed075acf40a68b781cdab1bb4610
prudhvir3ddy <prudhvi.reddy@target.com>
2021-08-12 01:08:20 +0530

    rm *fragment workers fcm

--- app/src/main/java/com/prudhvir3ddy/todo_app_gettingthingsdone/utils/IntentConstants.kt
+++
@@ -1,5 +0,0 @@
-package com.prudhvir3ddy.todo_app_gettingthingsdone.utils
-
-object IntentConstants {
-  const val TODO = "todo"
-}
---
+++ app/src/main/java/com/prudhvir3ddy/todo_app_gettingthingsdone/view/task/TaskType.kt
@@ -0,0 +1,4 @@
+package com.prudhvir3ddy.todo_app_gettingthingsdone.view.task
+
+enum class TaskType {
+}
