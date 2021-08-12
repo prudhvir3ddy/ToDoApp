76eabef35d83daa1421c7e70fb234bcc05b2d3cb
prudhvi reddy <prudhvireddy.m01@gmail.com>
2021-08-12 10:47:30 +0530

    removed firebase and worker deps  (#56)
    * removed the worker and firebase deps
    
    Co-authored-by: prudhvir3ddy <prudhvi.reddy@target.com>

--- app/src/main/java/com/prudhvir3ddy/todo_app_gettingthingsdone/ToDoApp.kt
+++ app/src/main/java/com/prudhvir3ddy/todo_app_gettingthingsdone/ToDoApp.kt
@@ -1,28 +1,18 @@
 package com.prudhvir3ddy.todo_app_gettingthingsdone
 
 import android.app.Application
-import androidx.hilt.work.HiltWorkerFactory
-import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 import timber.log.Timber.DebugTree
-import javax.inject.Inject
 
 @HiltAndroidApp
-class ToDoApp : Application(), Configuration.Provider {
-
-  @Inject lateinit var workerFactory: HiltWorkerFactory
+class ToDoApp : Application() {
 
   override fun onCreate() {
     super.onCreate()
-
 
     if (BuildConfig.DEBUG) {
       Timber.plant(DebugTree())
     }
   }
-
-  override fun getWorkManagerConfiguration() =
-    Configuration.Builder().setWorkerFactory(workerFactory).build()
-
 }
